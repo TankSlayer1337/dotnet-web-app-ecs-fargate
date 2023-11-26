976484cecaa21fca1a103d487fcefb577acc637e
André Säll <erikandresall@gmail.com>
2023-11-26 14:01:17 +0100

    Configure Kestrel to listen on port 8080.

--- src/WeatherApp/WeatherApp/Program.cs
+++ src/WeatherApp/WeatherApp/Program.cs
@@ -7,6 +7,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.WebHost.UseKestrel(options =>
+{
+    options.ListenAnyIP(8080);
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
